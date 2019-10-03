33dd1ceed15e0d34702dc0a7111cbbd997554317
Edward Harris <22172022+dwardrian@users.noreply.github.com>
2019-10-04 00:16:32 +0100

    initial

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueClipboard from "vue-clipboard2";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueClipboard);
+
 new Vue({
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
